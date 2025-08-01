ae7f3a6effdfb5d33d39ff756d2c77d4e9c7c0fd
abhinavv26 <abhinavrajeshsingh@gmail.com>
2025-08-01 14:52:05 +0530

    hello class

--- demo.c
+++ demo.c
@@ -6,5 +6,9 @@ int main()
      printf("Welcome to the world of programming");
      getch();
 
+     printf("Welcome to the world of programming");
+     printf("Welcome to the world of programming");
+     getch();
+     
      return 0;
 }
